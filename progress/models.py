0370b5614860f974e6dc83aead0f735690cc00e4
yasin-wajihaa <wajihaazafar@gmail.com>
2026-05-27 19:34:16 +0530

    feat: add model validation with clean method

--- progress/models.py
+++ progress/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from courses.models import Course, SyllabusItem
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from django.core.exceptions import ValidationError
 
 class Progress(models.Model):
     course = models.ForeignKey(Course, on_delete = models.CASCADE)
@@ -14,3 +14,11 @@ class Progress(models.Model):
 
     def __str__(self):
         return f'W{self.week:02}-{self.course}'
+    
+    def clean(self):
+        o = SyllabusItem.objects.get(course=self.course, week=self.week)
+        if (self.AQs_done > o.AQs) or (self.PAs_done > o.PAs) or (self.GAs_done > o.GAs) or (self.lecs_done > o.lectures) :
+            raise ValidationError (
+                'Ehhhh!?'
+            )
+        
