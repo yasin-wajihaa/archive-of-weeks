8f62e325829e46c3aab42c29656dbc66f0c54b47
yasin-wajihaa <wajihaazafar@gmail.com>
2026-05-27 05:47:17 +0530

    feat: add initial models and migrations

--- progress/models.py
+++ progress/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from courses.models import Course, SyllabusItem
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Progress(models.Model):
+    course = models.ForeignKey(Course, on_delete = models.CASCADE)
+    week = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(12)])
+
+    lecs_done = models.IntegerField(validators=[MinValueValidator(0)])
+    AQs_done = models.IntegerField(validators=[MinValueValidator(0)])
+    PAs_done = models.IntegerField(validators=[MinValueValidator(0)])
+    GAs_done = models.IntegerField(validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return f'W{self.week:02}-{self.course}'
