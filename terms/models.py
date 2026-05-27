66175408b3f7f4feb0f03b2363d6b4c5ec496684
yasin-wajihaa <wajihaazafar@gmail.com>
2026-05-27 05:46:27 +0530

    feat: add initial models and migrations

--- terms/models.py
+++ terms/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+def current_yr():
+    return str(date.today().year)[2:]
+
+term_code_choices = {
+    'F1': 'first_term',
+    'F2': 'second_term',
+    'F3': 'third_term'
+}
+
+class Term(models.Model):
+
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    yr = models.CharField(max_length=2, default=current_yr)  
+    code = models.CharField(max_length=2, choices = term_code_choices)
+
+    def __str__(self):
+        return f'{self.yr}{self.code}'
+    
+    class Meta:
+        unique_together = [
+            ['yr', 'code']
+        ]
