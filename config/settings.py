7a3793f53ba4ede26f4150e1eb52ad23d625aede
yasin-wajihaa <wajihaazafar@gmail.com>
2026-05-31 17:17:27 +0530

    feat: create dashboard app for views

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dashboard.apps.DashboardConfig',
     'users.apps.UsersConfig',
     'terms.apps.TermsConfig',
     'courses.apps.CoursesConfig',
--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 
 urlpatterns = [
+    path('', include('dashboard.urls')),
     path('terms/', include('terms.urls')),
     path('courses/', include('courses.urls')),
     path('progress/', include('progress.urls')),
