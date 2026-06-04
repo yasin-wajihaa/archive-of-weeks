f6ef82e60d8a9e81277965ec3878a704de40bb52
yasin-wajihaa <wajihaazafar@gmail.com>
2026-06-04 21:03:12 +0530

    feat: render homepage with dynamic context data

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from terms import utils
+
 
 def home(request):
-    return render(request, 'dashboard/home.html')
+    term = utils.current_term()
+    year = utils.current_year()
+    season = utils.current_season()
+    today = utils.today()
+    week = utils.current_week()
+    week_remaining = 12-week
+
+    return render(request, 'dashboard/home.html', {
+        'term': term,
+        'year': year,
+        'season': season,
+        'today_teehee': today,
+        'week': week,
+        'week_remaining': week_remaining,
+
+    })
---
+++ terms/utils.py
@@ -0,0 +1,30 @@
+from datetime import date
+from .models import Term
+from progress.models import Progress
+from django.db.models import Max
+
+def current_term():
+    term = Term.objects.filter(start_date__lte=date.today(), end_date__gte=date.today()).first()
+    return term
+
+def current_year():
+    return date.today().year
+
+def today():
+    return date.today()
+
+def current_season():
+    term = current_term()
+    t = str(term)[-1]
+    season_name = None
+    if t == '1':
+        season_name = 'Spring'
+    elif t == '2':
+        season_name = 'Summer'
+    else:
+        season_name = 'Fall'
+    return season_name
+
+def current_week():
+    week = Progress.objects.aggregate(Max("week")) or 1
+    return week['week__max']
