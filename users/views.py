0381763cb4de05f3d89d0e26f1299dfdc4be3e7b
yasin-wajihaa <wajihaazafar@gmail.com>
2026-06-09 00:23:09 +0530

    feat: add registration view and template

--- users/views.py
+++ users/views.py
@@ -1,6 +1,23 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.views.generic.edit import FormView
+from .models import UserProfile
+from django.db import transaction
+
 
 def index(request):
     return HttpResponse('on the users yeah!')
+
+class RegisterView(FormView):
+    template_name = 'users/register.html'
+    form_class = UserCreationForm
+    success_url = '/'
+
+    @transaction.atomic
+    def form_valid(self, form):
+        user = form.save()
+        UserProfile.objects.create(user=user)
+
+        return super().form_valid(form)
+
