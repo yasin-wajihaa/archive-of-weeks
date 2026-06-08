3bab38d08b47a8b96ef53aceab89d70e169e9b59
yasin-wajihaa <wajihaazafar@gmail.com>
2026-06-09 02:06:47 +0530

    feat: add email and pfp field in user register form

--- users/views.py
+++ users/views.py
@@ -6,12 +6,13 @@ from .models import UserProfile
 from django.db import transaction
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from .forms import UserRegisterForm
 
 def index(request):
     return HttpResponse('on the users yeah!')
 
 class RegisterView(SuccessMessageMixin, FormView):
-    form_class = UserCreationForm
+    form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = '/'
     success_message = 'User registered successfully!'
@@ -19,7 +20,9 @@ class RegisterView(SuccessMessageMixin, FormView):
     @transaction.atomic
     def form_valid(self, form):
         user = form.save()
-        UserProfile.objects.create(user=user)
+        pfp = form.cleaned_data['pfp']
+        UserProfile.objects.create(pfp=pfp, user=user)
+
 
         return super().form_valid(form)
 
