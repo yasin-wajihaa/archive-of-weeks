1d4c1fd5648dbd12ed3908917cd7f8d92d9b064d
yasin-wajihaa <wajihaazafar@gmail.com>
2026-06-09 01:22:13 +0530

    feat: display django flash messages in templates

--- users/views.py
+++ users/views.py
@@ -4,15 +4,17 @@ from django.http import HttpResponse
 from django.views.generic.edit import FormView
 from .models import UserProfile
 from django.db import transaction
-
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 
 def index(request):
     return HttpResponse('on the users yeah!')
 
-class RegisterView(FormView):
-    template_name = 'users/register.html'
+class RegisterView(SuccessMessageMixin, FormView):
     form_class = UserCreationForm
+    template_name = 'users/register.html'
     success_url = '/'
+    success_message = 'User registered successfully!'
 
     @transaction.atomic
     def form_valid(self, form):
